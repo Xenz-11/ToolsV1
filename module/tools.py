b54399d43addb53405a7fd67f75f4d2c1afd8efe
Inu Ganteng <102963507+Bang-Inu@users.noreply.github.com>
2022-04-04 22:11:23 +0700

    Update tools.py

--- module/tools.py
+++ module/tools.py
@@ -1,14 +1,15 @@
-## WARNA ASU #####
-P = '\x1b[1;97m' # PUTIH
-M = '\x1b[1;91m' # MERAH
-H = '\x1b[1;92m' # HIJAU
-K = '\x1b[1;93m' # KUNING
-B = '\x1b[1;94m' # BIRU
-U = '\x1b[1;95m' # UNGU
-O = '\x1b[1;96m' # BIRU MUDA
-N = '\x1b[0m'    # WARNA MATI
-##################
-
+######################
+## WARNA ASU ##### WARNA ##
+P = '\x1b[1;97m' # PUTIH #######
+M = '\x1b[1;91m' # MERAH ######
+H = '\x1b[1;92m' # HIJAU ######
+K = '\x1b[1;93m' # KUNING ####
+B = '\x1b[1;94m' # BIRU ######
+U = '\x1b[1;95m' # UNGU ######
+O = '\x1b[1;96m' # BIRU MUDA ###
+N = '\x1b[0m'      # WARNA MATI ###
+######################
+april =input
 import random
 import sys
 import time
@@ -20,40 +21,40 @@ def inu_ganteng_banget(s):
         time.sleep(random.random() * 0.1)
 
 
-user_reply = input("\x1b[1;92mNamamu Bang? \x1b[1;93m")
-inu_ganteng_banget('\x1b[1;92m[+]===============[ MENU ]===============[+]')
-inu_ganteng_banget('\x1b[1;96m1 Install Bahan')
-inu_ganteng_banget('2 Tampilan Termux Oh-My-Zsh')
-inu_ganteng_banget('3 Matematika')
-inu_ganteng_banget('4 Dark Multi Brute Force')
-inu_ganteng_banget('5 Tools MR-Xyaa')
-inu_ganteng_banget('6 Terkey')
-inu_ganteng_banget('7 Spamcall')
-inu_ganteng_banget('8 Spam By Bang Xenzi')
-inu_ganteng_banget('9 DB-BOM Spam By Bang Xenzi')
-inu_ganteng_banget('10 Spamer')
-inu_ganteng_banget('11 Red Hawk')
-inu_ganteng_banget('12 D-Tect')
-inu_ganteng_banget('13 Hunner')
-inu_ganteng_banget('14 WPScan')
-inu_ganteng_banget('15 Webdav')
-inu_ganteng_banget('16 Metasploit')
-inu_ganteng_banget('17 Kali Nethunter')
-inu_ganteng_banget('18 Ubuntu')
-inu_ganteng_banget('19 Youtube Dl')
-inu_ganteng_banget('20 viSQL')
-inu_ganteng_banget('21 Weeman')
-inu_ganteng_banget('22 WFDroid')
-inu_ganteng_banget('23 FBBrute')
-inu_ganteng_banget('24 Ngrok')
-inu_ganteng_banget('25 Torshammer')
-inu_ganteng_banget('26 RouterSploit')
-inu_ganteng_banget('27 Hydra')
-inu_ganteng_banget('28 Weevely')
-inu_ganteng_banget('29 SQLMap')
-inu_ganteng_banget('30 Dirbuster')
-inu_ganteng_banget('31 Pybelt')
-inu_ganteng_banget('\x1b[1;91m[00]___EXIT TO MENU')
-inu_ganteng_banget('\x1b[1;92m[+]===============[ MENU ]===============[+]')
+user_reply = april("\x1b[1;92mNamamu Bang? \x1b[1;93m")
+inu_ganteng_banget('\x1b[1;94m[+]===============[ MENU ]===============[+]')
+inu_ganteng_banget('\x1b[1;96m1 \x1b[1;92mInstall Bahan')
+inu_ganteng_banget('\x1b[1;96m2 \x1b[1;92mTampilan Termux Oh-My-Zsh')
+inu_ganteng_banget('\x1b[1;96m3 \x1b[1;92mMatematika')
+inu_ganteng_banget('\x1b[1;96m4 \x1b[1;92mDark Multi Brute Force')
+inu_ganteng_banget('\x1b[1;96m5\x1b[1;92m Tools MR-Xyaa')
+inu_ganteng_banget('\x1b[1;96m6\x1b[1;92m Terkey')
+inu_ganteng_banget('\x1b[1;96m7 \x1b[1;92mSpamcall')
+inu_ganteng_banget('\x1b[1;96m8 \x1b[1;92mSpam By Bang Xenzi')
+inu_ganteng_banget('\x1b[1;96m9 \x1b[1;92mDB-BOM Spam By Bang Xenzi')
+inu_ganteng_banget('\x1b[1;96m10 \x1b[1;92mSpamer')
+inu_ganteng_banget('\x1b[1;96m11 \x1b[1;92mRed Hawk')
+inu_ganteng_banget('\x1b[1;96m12 \x1b[1;92mD-Tect')
+inu_ganteng_banget('\x1b[1;96m13 \x1b[1;92mHunner')
+inu_ganteng_banget('\x1b[1;96m14 \x1b[1;92mWPScan')
+inu_ganteng_banget('\x1b[1;96m15 \x1b[1;92mWebdav')
+inu_ganteng_banget('\x1b[1;96m16\x1b[1;92m Metasploit')
+inu_ganteng_banget('\x1b[1;96m17\x1b[1;92m Kali Nethunter')
+inu_ganteng_banget('\x1b[1;96m18\x1b[1;92m Ubuntu')
+inu_ganteng_banget('\x1b[1;96m19 \x1b[1;92mYoutube Dl')
+inu_ganteng_banget('\x1b[1;96m20 \x1b[1;92mviSQL')
+inu_ganteng_banget('\x1b[1;96m21 \x1b[1;92mWeeman')
+inu_ganteng_banget('\x1b[1;96m22 \x1b[1;92mWFDroid')
+inu_ganteng_banget('\x1b[1;96m23\x1b[1;92m FBBrute')
+inu_ganteng_banget('\x1b[1;96m24 \x1b[1;92mNgrok')
+inu_ganteng_banget('\x1b[1;96m25\x1b[1;92m Torshammer')
+inu_ganteng_banget('\x1b[1;96m26\x1b[1;92m RouterSploit')
+inu_ganteng_banget('\x1b[1;96m27\x1b[1;92m Hydra')
+inu_ganteng_banget('\x1b[1;96m28 \x1b[1;92mWeevely')
+inu_ganteng_banget('\x1b[1;96m29 \x1b[1;92mSQLMap')
+inu_ganteng_banget('\x1b[1;96m30\x1b[1;92m Dirbuster')
+inu_ganteng_banget('\x1b[1;96m31 \x1b[1;92mPybelt')
+inu_ganteng_banget('\x1b[1;96m00 \x1b[1;91mEXIT TO MENU')
+inu_ganteng_banget('\x1b[1;94m[+]===============[ MENU ]===============[+]')
 print("\x1b[1;93mBang", user_reply)
 inu_ganteng_banget('\x1b[0mPilih Menunya Bang')
