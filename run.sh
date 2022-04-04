627dd10fe158a8b21e42fb2e0cee31f344f16ede
Inu Ganteng <102963507+Bang-Inu@users.noreply.github.com>
2022-04-04 22:12:55 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@
 """
 MIT License
 
-Copyright (c) 2022 MR-Xyaa
+Copyright (c) 2022 Bang-Inu
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
