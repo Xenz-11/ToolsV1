5d83a87439e143e1a76e6694c85a4367f35c1d5f
Inu Ganteng <102963507+Bang-Inu@users.noreply.github.com>
2022-04-04 18:38:48 +0700

    Create logo.py

---
+++ module/logo.py
@@ -0,0 +1,31 @@
+###################
+## ANAK BABI KAU ##
+###################
+## WARNA ASU #####
+P = '\x1b[1;97m' # PUTIH
+M = '\x1b[1;91m' # MERAH
+H = '\x1b[1;92m' # HIJAU
+K = '\x1b[1;93m' # KUNING
+B = '\x1b[1;94m' # BIRU
+U = '\x1b[1;95m' # UNGU
+O = '\x1b[1;96m' # BIRU MUDA
+N = '\x1b[0m'    # WARNA MATI
+##################
+logo =("""
+\x1b[1;92m╔═══════[  \x1b[1;91mMR-Xyaa \x1b[1;92m]═════════════════════════════╗
+\x1b[1;92m■  \x1b[1;93m[▪]   \x1b[1;94mAuthor : BangInu                        \x1b[1;92m■
+\x1b[1;92m■  \x1b[1;93m[▪]   \x1b[1;94mFacbook : Sapa Yah                      \x1b[1;92m■
+\x1b[1;92m■  \x1b[1;93m[▪]   \x1b[1;94mGithub : github.com/Bang-Inu            \x1b[1;92m■
+\x1b[1;92m╚═══════[   XXXXXX ]═════════════════════════════╝
+""")
+
+import itertools
+import threading
+import time
+import sys
+import os
+
+inu =print
+def kntl():
+      os.system('clear')
+inu (logo)
