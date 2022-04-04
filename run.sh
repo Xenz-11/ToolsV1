5e722e587dcd45bc59c069c63e57c143e516066f
Inu Ganteng <102963507+Bang-Inu@users.noreply.github.com>
2022-04-04 18:31:50 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,234 @@
+#!/system/xbin/bash/sh/python
+#MauNgapainGblk?
+#DiRecodeGakBakalJadiinLuMastah
+#DiGantiAuthorGakBakalJadiinLuMaster
+#MikirMakeOtakGblk
+#KontolKaloMauReedit/Recode 
+#Pm Gua Bangsat
+"""
+MIT License
+
+Copyright (c) 2022 MR-Xyaa
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+cd module
+clear
+python loading.py
+sleep 3
+clear
+python logo.py
+python tools.py
+
+biru='\033[34;1m'
+ijo='\033[32;1m'  
+ungu='\033[35;1m'
+bm='\033[36;1m'
+abang='\033[31;1m'
+putih='\033[37;1m'                                           
+kuning='\033[33;1m'
+sleep 1
+
+
+read -p "Pilih No:" bro
+
+## MENU1
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f slant "MR-Xyaa";
+pkg update && pkg upgrade
+
+pkg install python python2 python3
+
+pip install requests mechanize
+
+pip2 install requests mechanize
+
+pkg install git
+
+pkg install ruby
+
+pkg install curl
+
+gem install lolcat
+
+pip2 install php
+
+pkg install figlet
+
+pkg install wget
+
+pkg install nano
+
+pkg install bash
+
+pkg install cowsay
+
+apt update && apt upgrade -y
+
+pkg install bash
+
+pkg install pv -y
+
+pkg install wget -y
+
+pkg install git -y
+fi
+
+## Menu2
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet -f slant "MR-Xyaa";
+git clone https://github.com/MR-Xyaa/Oh-My-Zsh
+cd Oh-My--Zsh
+git pull
+sh oh-my-zsh
+fi
+
+## MENU3
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+toilet -f slant "MR-Xyaa";
+git clone https://github.com/MR-Xyaa/MTK
+cd MTK
+git pull
+sh mtk.sh
+fi
+
+## MENU4
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+toilet -f slant "MR-Xyaa";
+git clone https://github.com/MR-Xyaa/dmbf
+cd dmbf
+git pull
+python dmbf.py
+fi
+
+
+## MENU5
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+toilet -f slant "MR-Xyaa";
+pkg update
+pkg upgrade
+pkg install git
+clear
+git clone https://github.com/MR-Xyaa/a
+cd a
+git pull
+sh a.sh
+fi
+
+## MENU6
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+toilet -f slant "MR-Xyaa";
+git clone https://github.com/MR-Xyaa/terkey
+cd terkey
+git pull
+python terkey.py
+fi
+
+## MENU7
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+toilet -f slant "MR-Xyaa";
+git clone https://github.com/MR-Xyaa/spamcall
+cd spamcall
+git pull
+sh spam-call.sh
+fi
+
+## MENU8
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+toilet -f slant "MR-Xyaa";
+git clone https://github.com/Xenzi-XN1/Tools-Spam
+cd Tools-Spam
+git pull
+python Menu.py
+fi
+
+## MENU0
+if [ $bro = 00 ] || [ $bro = 00 ]
+then
+clear
+toilet -f slant "MR-Xyaa";
+sleep 1
+echo -e $abang"We Security"
+sleep 1
+echo -e $biru"We Not Friends"
+sleep 1
+echo -e $bm"We Are Family"
+sleep 1
+echo -e $kuning"Hacking Is Not Criminal"
+sleep 1
+echo -e $ungu"Ketika Sebuah Hayalan Tidak tercapai"
+sleep 1
+echo -e $putih"Maka Terus lah BerJuang Dan Berusaha"
+sleep 1
+echo -e $kuning"Karna Suatu Hari Nanti Kamu akan Mendapatkannya"
+sleep 1
+echo -e $ijo"MR-Xyaa"
+sleep 5
+python tai.py
+sleep 5
+clear
+exit
+fi
+
+# MENU9
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+toilet -f slant "MR-Xyaa";
+pkg update && pkg upgrade
+pkg install nano
+pkg install git
+pkg install python
+pip install requests
+git clone https://github.com/Xenzi-XN1/DB-Bom
+cd DB-Bom
+python DB.py
+fi
+
+## MENU10
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+toilet -f slant "MR-Xyaa";
+git clone https://github.com/MR-Xyaa/Spamer
+cd Spamer
+git pull
+bash mulai.sh
+fi
+          else:
+             echo -e $abang"pilih yg bener setan"
